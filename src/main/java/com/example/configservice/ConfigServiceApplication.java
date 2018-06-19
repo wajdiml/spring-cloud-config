b2f811dc9bcb03f680d2a31a7ef509ed0453aebd
Mlichi <wajdi.mlichi@cgi.com>
2018-06-19 13:05:56 -0400

    first

---
+++ src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -0,0 +1,21 @@
+package com.example.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConfigServiceApplication.class);
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
